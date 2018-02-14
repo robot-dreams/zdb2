96fa4269bf383736f434e44f8d782c2696c64967
Elliott Jin <eyj@dropbox.com>
2018-02-13 21:27:39 -0800

    Add helper function for generating joined table headers

---
+++ utils.go
@@ -0,0 +1,61 @@
+package zdb2
+
+import (
+	"fmt"
+
+	"github.com/dropbox/godropbox/errors"
+)
+
+func JoinedHeader(
+	t1 *TableHeader,
+	t2 *TableHeader,
+	joinField1 string,
+	joinField2 string,
+) (*TableHeader, error) {
+	if !hasField(t1, joinField1) {
+		return nil, errors.Newf("%v does not have field %v", *t1, joinField1)
+	}
+	if !hasField(t2, joinField2) {
+		return nil, errors.Newf("%v does not have field %v", *t2, joinField2)
+	}
+	joinedName := fmt.Sprintf(
+		"(%s.%s = %s.%s)", t1.Name, joinField1, t2.Name, joinField2)
+	return &TableHeader{
+		Name:   joinedName,
+		Fields: append(qualifiedFields(t1), qualifiedFields(t2)...),
+	}, nil
+}
+
+func hasField(t *TableHeader, fieldName string) bool {
+	for _, field := range t.Fields {
+		if field.Name == fieldName {
+			return true
+		}
+	}
+	return false
+}
+
+// Prepends the table name and "." to each field name for disambiguation.  For
+// example, "id" in the "user" table becomes "user.id".
+func qualifiedFields(t *TableHeader) []*Field {
+	result := make([]*Field, 0, len(t.Fields))
+	for _, field := range t.Fields {
+		result = append(
+			result,
+			&Field{
+				Name: t.Name + "." + field.Name,
+				Type: field.Type,
+			})
+	}
+	return result
+}
+
+func CloseAll(iters []Iterator) (firstErr error) {
+	for _, iter := range iters {
+		err := iter.Close()
+		if err != nil && firstErr == nil {
+			firstErr = err
+		}
+	}
+	return
+}
---
+++ utils_test.go
@@ -0,0 +1,42 @@
+package zdb2
+
+import (
+	. "gopkg.in/check.v1"
+)
+
+type UtilsSuite struct{}
+
+var _ = Suite(&UtilsSuite{})
+
+func (s *UtilsSuite) TestJoinedHeader(c *C) {
+	t1 := &TableHeader{
+		Name: "users",
+		Fields: []*Field{
+			{"id", Int32},
+			{"name", String},
+		},
+	}
+	t2 := &TableHeader{
+		Name: "logins",
+		Fields: []*Field{
+			{"user_id", Int32},
+			{"timestamp", Int32},
+			{"client", String},
+		},
+	}
+	joined, err := JoinedHeader(t1, t2, "id", "user_id")
+	c.Assert(err, IsNil)
+	c.Assert(
+		joined,
+		DeepEquals,
+		&TableHeader{
+			Name: "(users.id = logins.user_id)",
+			Fields: []*Field{
+				{"users.id", Int32},
+				{"users.name", String},
+				{"logins.user_id", Int32},
+				{"logins.timestamp", Int32},
+				{"logins.client", String},
+			},
+		})
+}
