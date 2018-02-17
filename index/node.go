979c41546d874a2ec5d14cb567eaf3c72d0fe15d
Elliott Jin <eyj@dropbox.com>
2018-02-17 11:06:29 -0800

    Fix comment about addEntry

--- index/node.go
+++ index/node.go
@@ -24,8 +24,8 @@ type node interface {
 	flush() error
 
 	// addEntry will write changes to the block buffer before returning.  If
-	// adding an entry caused the node to split, then the returned router will
-	// be non-nil.
+	// adding an entry requires the node to be updated, then the returned router
+	// will be non-nil.
 	addEntry(Entry) (*router, error)
 
 	findEqual(key int32) (Iterator, error)
