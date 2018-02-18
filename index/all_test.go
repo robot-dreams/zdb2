a720782b4c1ba788f63dbc8af3befdfb0dd32fb2
Elliott Jin <eyj@dropbox.com>
2018-02-18 13:03:59 -0800

    B+ tree test shouldn't depend on order of AddEntry calls

--- index/all_test.go
+++ index/all_test.go
@@ -1,12 +1,117 @@
 package index
 
 import (
+	"io"
 	"testing"
 
 	. "gopkg.in/check.v1"
+
+	. "github.com/dropbox/godropbox/gocheck2"
 )
 
 // Initialize gocheck.
 func Test(t *testing.T) {
 	TestingT(t)
+}
+
+// Distance between keys for test entries.
+var keyDelta int32 = 5
+
+func generateTestEntry(key int32, offset int) Entry {
+	return Entry{
+		Key: key,
+		RID: RecordID{
+			PageID: int32(offset),
+			SlotID: uint16(offset),
+		},
+	}
+}
+
+func generateSortedTestEntries(numKeys, numEntriesPerKey int) []Entry {
+	testEntries := make([]Entry, 0, numKeys*numEntriesPerKey)
+	for i := 0; i < numKeys; i++ {
+		for j := 0; j < numEntriesPerKey; j++ {
+			testEntries = append(
+				testEntries,
+				generateTestEntry(int32(i)*keyDelta, j))
+		}
+	}
+	return testEntries
+}
+
+func checkTestEntries(c *C, tree *bPlusTree, numKeys int, numEntriesPerKey int) {
+	for i := 0; i < numKeys; i++ {
+		// All of these should be found.
+		iter, err := tree.FindEqual(int32(i) * keyDelta)
+		c.Assert(err, IsNil)
+		var expectedEntries []Entry
+		for j := 0; j < numEntriesPerKey; j++ {
+			expectedEntries = append(
+				expectedEntries,
+				generateTestEntry(int32(i)*keyDelta, j))
+		}
+		checkIterator(c, iter, expectedEntries)
+
+		// None of these should be found.
+		iter, err = tree.FindEqual(int32(i)*keyDelta + 1)
+		c.Assert(err, IsNil)
+		checkIterator(c, iter, nil)
+	}
+
+	// Use FindGreaterEqual to iterate through the second half of the entries.
+	start := numKeys / 2
+	iter, err := tree.FindGreaterEqual(int32(start) * keyDelta)
+	c.Assert(err, IsNil)
+	var expectedEntries []Entry
+	for i := start; i < numKeys; i++ {
+		for j := 0; j < numEntriesPerKey; j++ {
+			expectedEntries = append(
+				expectedEntries,
+				generateTestEntry(int32(i)*keyDelta, j))
+		}
+	}
+	checkIterator(c, iter, expectedEntries)
+
+	// If the key is greater than anything ever added, then there should be no
+	// entries in the returned iterator.
+	iter, err = tree.FindGreaterEqual(int32(numKeys) * keyDelta)
+	c.Assert(err, IsNil)
+	checkIterator(c, iter, nil)
+}
+
+func checkIterator(c *C, iter Iterator, expected []Entry) {
+	var actual []Entry
+	for {
+		entry, err := iter.Next()
+		if err == io.EOF {
+			break
+		}
+		c.Assert(err, IsNil)
+		actual = append(actual, entry)
+	}
+
+	// Repeated calls to Next should continue to return io.EOF after the
+	// reaching the end of the Iterator.
+	_, err := iter.Next()
+	c.Assert(err, Equals, io.EOF)
+	_, err = iter.Next()
+	c.Assert(err, Equals, io.EOF)
+
+	// The correct number of entries should have been returned.
+	c.Assert(len(actual), Equals, len(expected))
+
+	// The returned entries should be sorted by key.
+	for i := 1; i < len(actual); i++ {
+		c.Assert(actual[i-1].Key <= actual[i-1].Key, IsTrue)
+	}
+
+	// The set of returned entries should match the expected entries.
+	expectedSet := make(map[Entry]struct{})
+	for _, entry := range expected {
+		expectedSet[entry] = struct{}{}
+	}
+	for _, entry := range actual {
+		_, ok := expectedSet[entry]
+		c.Assert(ok, IsTrue)
+	}
 }
--- index/b_plus_tree_test.go
+++ index/b_plus_tree_test.go
@@ -1,8 +1,7 @@
 package index
 
 import (
-	"io"
-
+	"github.com/dropbox/godropbox/math2/rand2"
 	. "gopkg.in/check.v1"
 )
 
@@ -10,90 +9,29 @@ type BPlusTreeSuite struct{}
 
 var _ = Suite(&BPlusTreeSuite{})
 
-func checkIterator(c *C, iter Iterator, expected []Entry) {
-	// Ensure that the Iterator contains exactly the expected Records.
-	for _, entry := range expected {
-		actual, err := iter.Next()
-		c.Assert(err, IsNil)
-		c.Assert(actual, DeepEquals, entry)
-	}
-	_, err := iter.Next()
-	c.Assert(err, Equals, io.EOF)
-	// Repeated calls to Next should continue to return io.EOF after the
-	// reaching the end of the Iterator.
-	_, err = iter.Next()
-	c.Assert(err, Equals, io.EOF)
-	_, err = iter.Next()
-	c.Assert(err, Equals, io.EOF)
-}
+type entryShuffle []Entry
+
+var _ rand2.Swapper = (entryShuffle)(nil)
+
+func (e entryShuffle) Len() int      { return len(e) }
+func (e entryShuffle) Swap(i, j int) { e[i], e[j] = e[j], e[i] }
 
 func (s *BPlusTreeSuite) TestBPlusTree(c *C) {
 	path := c.MkDir() + "/b_plus_tree"
 	tree, err := OpenBPlusTree(path)
 	c.Assert(err, IsNil)
-	expectedEntry := func(key int32, offset int) Entry {
-		return Entry{
-			Key: key,
-			RID: RecordID{
-				PageID: int32(offset),
-				SlotID: uint16(offset),
-			},
-		}
-	}
-	d := int32(5)
 	numKeys := 100
 	numEntriesPerKey := 10
-	keys := make(map[int32]struct{})
-	for i := 0; i < numKeys; i++ {
-		keys[int32(i)*d] = struct{}{}
-	}
+	testEntries := generateSortedTestEntries(numKeys, numEntriesPerKey)
 
-	// Reading the keys out of a map randomizes the iteration order each time we
-	// run the test and lets us check more cases.
-	for key := range keys {
-		for i := 0; i < numEntriesPerKey; i++ {
-			err = tree.AddEntry(expectedEntry(key, i))
-			c.Assert(err, IsNil)
-		}
-	}
-	for i := 0; i < numKeys; i++ {
-		// All of these should be found.
-		iter, err := tree.FindEqual(int32(i) * d)
+	// Add test entries in random order.
+	rand2.Shuffle(entryShuffle(testEntries))
+	for _, entry := range testEntries {
+		err = tree.AddEntry(entry)
 		c.Assert(err, IsNil)
-		var expectedEntries []Entry
-		for j := 0; j < numEntriesPerKey; j++ {
-			expectedEntries = append(
-				expectedEntries,
-				expectedEntry(int32(i)*d, j))
-		}
-		checkIterator(c, iter, expectedEntries)
-
-		// None of these should be found.
-		iter, err = tree.FindEqual(int32(i)*d + 1)
-		c.Assert(err, IsNil)
-		checkIterator(c, iter, nil)
 	}
 
-	// Use FindGreaterEqual to iterate through the second half of the entries.
-	start := numKeys / 2
-	iter, err := tree.FindGreaterEqual(int32(start) * d)
-	c.Assert(err, IsNil)
-	var expectedEntries []Entry
-	for i := start; i < numKeys; i++ {
-		for j := 0; j < numEntriesPerKey; j++ {
-			expectedEntries = append(
-				expectedEntries,
-				expectedEntry(int32(i)*d, j))
-		}
-	}
-	checkIterator(c, iter, expectedEntries)
-
-	// If the key is greater than anything ever added, then there should be no
-	// entries in the returned iterator.
-	iter, err = tree.FindGreaterEqual(int32(numKeys) * d)
-	c.Assert(err, IsNil)
-	checkIterator(c, iter, nil)
-
+	checkTestEntries(c, tree, numKeys, numEntriesPerKey)
 	err = tree.Close()
 	c.Assert(err, IsNil)
 }
