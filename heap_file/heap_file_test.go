54f286f552ccd7fb60735293cf548be4b2433b4f
Elliott Jin <eyj@dropbox.com>
2018-02-19 18:20:23 -0800

    Add test for deletes

--- heap_file/heap_file_test.go
+++ heap_file/heap_file_test.go
@@ -101,3 +101,60 @@ func (s *HeapFileSuite) TestInsertAndGet(c *C) {
 	})
 	c.Assert(err, NotNil)
 }
+
+func (s *HeapFileSuite) TestDelete(c *C) {
+	path := c.MkDir() + "/heap_file_test"
+	t := &zdb2.TableHeader{
+		Name: "movies",
+		Fields: []*zdb2.Field{
+			{"title", zdb2.String},
+			{"rating", zdb2.Float64},
+			{"views", zdb2.Int32},
+		},
+	}
+	hf, err := NewHeapFile(path, t)
+	c.Assert(err, IsNil)
+
+	// Insert the expected records.
+	records := []zdb2.Record{
+		{"Leon: The Professional", 4.6, int32(2)},
+		{"Gattaca", 4.5, int32(2)},
+		{"Hackers", 3.7, int32(3)},
+		{"Inside Out", 4.7, int32(3)},
+	}
+	numToInsert := 5000
+	recordIDs := make([]zdb2.RecordID, numToInsert)
+	for i := 0; i < numToInsert; i++ {
+		recordID, err := hf.Insert(records[i%len(records)])
+		c.Assert(err, IsNil)
+		recordIDs[i] = recordID
+	}
+
+	// Make sure we've added enough test data to check an "interesting" case.
+	c.Assert(hf.bf.NumBlocks > 2, IsTrue)
+
+	// Make sure that some of the records we've added can be found.
+	start := rand2.Intn(len(recordIDs) - 10)
+	for i := start; i < start+10; i++ {
+		record, err := hf.Get(recordIDs[i])
+		c.Assert(err, IsNil)
+		c.Assert(record.Equals(records[i%len(records)]), IsTrue)
+	}
+
+	// Delete these records.
+	for i := start; i < start+10; i++ {
+		err = hf.Delete(recordIDs[i])
+		c.Assert(err, IsNil)
+
+		// Deletes should be idempotent.
+		err = hf.Delete(recordIDs[i])
+		c.Assert(err, IsNil)
+	}
+
+	// Trying to load deleted records should return (nil, nil).
+	for i := start; i < start+10; i++ {
+		record, err := hf.Get(recordIDs[i])
+		c.Assert(err, IsNil)
+		c.Assert(record, IsNil)
+	}
+}
